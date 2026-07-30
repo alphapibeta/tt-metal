f43305e96b335ced14be8b8b440bc4157f17e2b2
riverwuTT <riverwu@tenstorrent.com>
2026-07-30 23:11:01 +0000

    moving tensor to the main API folder

--- tests/tt_metal/tt_metal/test_bmm.cpp
+++ tests/tt_metal/tt_metal/test_bmm.cpp
@@ -16,7 +16,7 @@
 #include <tt-metalium/distributed.hpp>
 #include <tt-metalium/tilize_utils.hpp>
 #include <tt-metalium/experimental/metal2_host_api/program.hpp>
-#include <tt-metalium/experimental/tensor/mesh_tensor.hpp>
+#include <tt-metalium/tensor/mesh_tensor.hpp>
 #include "test_gold_impls.hpp"
 #include "impl/data_format/bfloat16_utils.hpp"
 
--- tt_metal/impl/tensor/spec/tensor_spec.cpp
+++ tt_metal/impl/tensor/spec/tensor_spec.cpp
@@ -2,10 +2,10 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <tt-metalium/experimental/tensor/spec/tensor_spec.hpp>
-#include <tt-metalium/experimental/tensor/tensor_types.hpp>
+#include <tt-metalium/tensor/spec/tensor_spec.hpp>
+#include <tt-metalium/tensor/tensor_types.hpp>
 #include <tt-metalium/experimental/per_core_allocation/memory_config.hpp>
-#include <tt-metalium/experimental/tensor/spec/memory_config/memory_config.hpp>
+#include <tt-metalium/tensor/spec/memory_config/memory_config.hpp>
 
 #include "layout/page_config_impl.hpp"
 #include "layout/tensor_layout_impl.hpp"
