87cc85c31ef2b5248749d9569e6df578dca53e0e
Alexey Zaytsev <ctr-azaytsev@ext.tenstorrent.com>
2026-07-31 14:00:40 +0000

    Reservation of memory for vector beforehand

--- tt_metal/llrt/tunnels_from_mmio_device.cpp
+++ tt_metal/llrt/tunnels_from_mmio_device.cpp
@@ -46,6 +46,7 @@ std::map<ChipId, std::vector<std::vector<ChipId>>> discover_tunnels_from_mmio_de
               continue;
           }
 
+          tunnels_from_mmio.reserve(all_eth_connections.at(mmio_chip_id).size());
           for (const auto& [eth_chan, connected_chip_chan] : all_eth_connections.at(mmio_chip_id)) {
               const auto& other_chip_id = std::get<0>(connected_chip_chan);
               if (device_ids.contains(other_chip_id)) {
@@ -91,7 +92,7 @@ std::map<ChipId, std::vector<std::vector<ChipId>>> discover_tunnels_from_mmio_de
               }
               dev_vec.insert(dev_vec.begin(), mmio_chip_id);
           }
-          tunnels_from_mmio_device.insert({mmio_chip_id, tunnels_from_mmio});
+          tunnels_from_mmio_device.insert({mmio_chip_id, std::move(tunnels_from_mmio)});
       }
 
       return tunnels_from_mmio_device;
